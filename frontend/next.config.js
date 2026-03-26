2033627999f0a75099d6b7073c37bd0d071ad492
yerbol2233 <130932521+yerbol2233@users.noreply.github.com>
2026-03-26 20:13:47 +0700

    подготовлен к деплою на вдс

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone" нужен только для Docker-деплоя.
-  // Для Vercel — убираем (Vercel сам управляет билдом).
+  output: "standalone",
   reactStrictMode: true,
 };
 
